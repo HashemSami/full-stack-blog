17fe4a87c226b29e115ec131f489bde69ecd8042
Hashem <hashem.sf@gmail.com>
2022-01-31 20:19:04 +0300

    adding clients

---
+++ client2/app/Main.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import ReactDOM from "react-dom";
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>Hello abu ah hesh</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#app"));
